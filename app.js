c98ee3ae97693851641a0ff89e503972a7a90845
Daniel Janda <87991817+LaterFriend@users.noreply.github.com>
2022-11-14 00:28:46 +0100

    1

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
-function add(num1, num2) {
-    return num1 + num2;
-}
-var number1 = 5;
-var number2 = "3";
-var result = add(number1, number2);
-console.log(result);
+var person = {
+    name: "Daniel",
+    age: 23
+};
+console.log(person);
--- app.ts
+++ app.ts
@@ -1,9 +1,9 @@
-function add(num1:number, num2:number) {
-    return num1 + num2;
-}
+const person: {
+    name: string;
+    age: number;
+} = {
+    name: "Daniel",
+    age: 23
+};
 
-const number1 = 5;
-const number2 = 3;
-
-const result = add(number1, number2);
-console.log(result);
+console.log(person);
